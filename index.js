13ff9448ed75f2630da2eba3e6ffdb50173136b2
faderz <58748338+faderzz@users.noreply.github.com>
2023-02-20 13:31:25 +0000

    logout command

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const mineflayer = require('mineflayer');
+const sqlite3 = require('sqlite3').verbose();
 const readline = require('readline');
 const Vec3 = require('vec3');
-const sqlite3 = require('sqlite3').verbose();
 require('dotenv').config();
 
 const username = process.env.email;
 const password = process.env.password;
 const server_address = process.env.server_address;
+
+const db = new sqlite3.Database('chat_messages.db', (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the chat_messages database.');
+  db.run(`CREATE TABLE IF NOT EXISTS messages (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            message TEXT NOT NULL,
+            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+          )`,
+    (err) => {
+      if (err) {
+        console.error(err.message);
+      }
+      console.log('Created messages table if it did not exist.');
+    });
+});
 
 const bot = mineflayer.createBot({
   host: server_address,
@@ -16,38 +35,29 @@ const bot = mineflayer.createBot({
   auth: 'microsoft'
 });
 
-const db = new sqlite3.Database('chatlog.db', (err) => {
-  if (err) {
-    console.error(err.message);
-  } else {
-    console.log('Connected to the chatlog database.');
-    db.run('CREATE TABLE IF NOT EXISTS chatlog (timestamp INTEGER, username TEXT, message TEXT)');
-  }
-});
-
 bot.once('spawn', () => {
   console.log(`Logged in as ${bot.username} on ${server_address}`);
   handleConsoleInput(bot);
-  bot.setControlState('forward', true)
-  bot.setControlState('jump', true)
-  bot.setControlState('sneak', true)
-});
-
-bot.on('respawn', () => {
-  bot.setControlState('forward', true)
-  bot.setControlState('jump', true)
-  bot.setControlState('sneak', true)
 });
 
 bot.on('chat', (username, message) => {
-  if (username === bot.username) return;
-  db.run('INSERT INTO chatlog (timestamp, username, message) VALUES (?, ?, ?)', [Date.now(), username, message], (err) => {
-    if (err) {
-      console.error(err.message);
-    } else {
-      console.log(`Saved chat message from ${username}: ${message}`);
-    }
-  });
+  if (username === bot.username) {
+    // Ignore messages sent by the bot itself
+    return;
+  }
+  console.log(`${username}: ${message}`);
+  saveChatMessageToDatabase(username, message);
+});
+
+bot.on('death', () => {
+  console.log('Bot died.');
+  bot.setControlState('jump', true);
+  bot.respawn();
+});
+
+bot.on('respawn', () => {
+  console.log('Bot respawned.');
+  bot.setControlState('forward', true);
 });
 
 function handleConsoleInput(bot) {
@@ -57,6 +67,26 @@ function handleConsoleInput(bot) {
   });
 
   rl.on('line', (input) => {
-    bot.chat(input);
+    if (input === '_logout') {
+      console.log('Logging out...');
+      db.close((err) => {
+        if (err) {
+          console.error(err.message);
+        }
+        console.log('Closed the chat_messages database.');
+        bot.quit();
+      });
+    } else {
+      bot.chat(input);
+    }
+  });
+}
+
+function saveChatMessageToDatabase(username, message) {
+  db.run(`INSERT INTO messages (username, message) VALUES (?, ?)`, [username, message], (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log(`Saved chat message from ${username} to the database.`);
   });
 }
