d2f0de14abfbbd05676c6b7e4715236a94b3cfd7
faderz <58748338+faderzz@users.noreply.github.com>
2023-02-20 13:13:39 +0000

    huh

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const mineflayer = require('mineflayer');
-require('dotenv').config()
+const readline = require('readline');
+const Vec3 = require('vec3');
+require('dotenv').config();
 
 const username = process.env.email;
 const password = process.env.password;
@@ -13,25 +15,37 @@ const bot = mineflayer.createBot({
   auth: 'microsoft'
 });
 
+function walkForward(bot, distance) {
+    const mcData = require('minecraft-data')(bot.version);
+    const movements = new Movements(bot, mcData);
+    
+    const dest = bot.entity.position.offset(0, 0, distance);
+    bot.pathfinder.setMovements(movements);
+    bot.pathfinder.setGoal(new GoalBlock(dest.x, dest.y, dest.z));
+  }
+
 bot.once('spawn', () => {
   console.log(`Logged in as ${bot.username} on ${server_address}`);
-  antiAfk(bot);
+  walkForward(bot, 5);
+  handleConsoleInput(bot);
+  bot.setControlState('jump', true)
+  bot.setControlState('sneak', true)
 });
 
-function antiAfk(bot) {
-    setInterval(() => {
-      const rand = Math.floor(Math.random() * 10);
-      if (rand < 5) {
-        bot.setControlState('forward', true);
-      } else {
-        bot.clearControlStates();
-      }
-      if (rand === 0) {
-        bot.setControlState('jump', true);
-        setTimeout(() => {
-          bot.setControlState('jump', false);
-        }, 500);
-      }
-    }, 5 * 60 * 1000);
-  }
-  
+bot.on('respawn', () => {
+    // Walk 5 blocks forward on respawn
+    walkForward(bot, 5);
+    bot.setControlState('jump', true)
+    bot.setControlState('sneak', true)
+  });
+
+function handleConsoleInput(bot) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.on('line', (input) => {
+    bot.chat(input);
+  });
+}
